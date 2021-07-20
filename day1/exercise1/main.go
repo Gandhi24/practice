f1c2d5b87390f9419cc3fc44567703ad2a83d2de
piyush.mishra <piyush.mishra2406@gmail.com>
2021-07-20 18:16:22 +0530

    add solutions for all 3 exercises

--- day1/exercise1/main.go
+++ day1/exercise1/main.go
@@ -13,6 +13,27 @@ func (m Matrix) Cols() int {
 	return m.cols
 }
 
-func main() {
+func (m *Matrix) setElement(i, j, val int) {
+	m.grid[i][j] = val
+}
 
+func (m *Matrix) add(other *Matrix) {
+	for row := 0; row < m.rows; row++ {
+		for col := 0; col < m.cols; col++ {
+			m.grid[row][col] += other.grid[row][col]
+		}
+	}
+}
+
+func main() {
+	var m *Matrix = &Matrix{
+		rows: 2,
+		cols: 4,
+		grid: [][]int{
+			{0, 1, 2, 3},
+			{4, 5, 6, 7},
+		},
+	}
+
+	m.setElement(0, 0, 100)
 }
